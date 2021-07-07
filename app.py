6bdcd2c1806a598c3c57ed3a9ada4669357e177b
amapic <amaury.pichat@gmail.com>
2021-07-07 19:40:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, jsonify
 
 @app.route('/api/meteo/')
 def meteo():
-    dictionnaire = {
-        'type': 'Prévision de température',
-        'valeurs': [24, 24, 25, 26, 27, 28],
-        'unite': "degrés Celcius"
-    }
-    return jsonify(dictionnaire)
+    return 'tt'
