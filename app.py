24b9741d14a4196c1b519aa03ec7865696dc4b5e
amapic <amaury.pichat@gmail.com>
2021-07-08 22:16:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask
+app = Flask(__name__)
 
-@app.route('/a')
-def meteo():
-	print('ee')
-	return "hello"
+@app.route("/")
+def hello():
+    return "Hello, World!"
